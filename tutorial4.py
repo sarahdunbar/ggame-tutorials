7a9c3c1dedcc377a551e42706fb37c3ef2eb503e
sarahdunbar <sarahdunbar@hanovernorwichschools.org>
2015-10-13 14:22:27 -0400

    Updated from Brython Server: 10/13/2015 2:22:27 PM

--- tutorial4.py
+++ tutorial4.py
@@ -29,34 +29,43 @@ class SpaceGame(App):
         self.vx = 1
         self.vy = 1
         self.vr = 0.01
+        self.thrust = 0
+        self.thrustframe = 1
+        SpaceGame.listenKeyEvent("keydown", "space", self.thrustOn)
+        SpaceGame.listenKeyEvent("keyup", "space", self.thrustOff)
+        self.fxcenter = self.fycenter = 0.5
         
         def step(self):
             self.x += self.vx
             self.y += self.vy
             self.rotation += self.vr
-        
-        self.thrust = 0
-        self.thrustframe = 1
-        SpaceGame.listenKeyEvent("keydown", "space", self.thrustOn)
-        SpaceGame.listenKeyEvent("keyup", "space", self.thrustOff)
-        
+            if self.thrust == 1:
+                self.setImage(self.thrustframe)
+                self.thrustframe += 1
+                if self.thrustframe == 4:
+                    self.thrustframe = 1
+            else:
+                self.setImage(0)
+                
         def thrustOn(self, event):
             self.thrust = 1
 
         def thrustOff(self, event):
             self.thrust = 0
-            
-        if self.thrust == 1:
-            self.setImage(self.thrustframe)
-            self.thrustframe += 1
-            if self.thrustframe == 4:
-                self.thrustframe = 1
-        else:
-            self.setImage(0)
-            
+    
+    def __init__(self, width, height):
+        super().__init__(width, height)
+        black = Color(0, 1)
+        noline = LineStyle(0, black)
+        bg_asset = RectangleAsset(width, height, noline, black)
+        bg = Sprite(bg_asset, (0,0))
+        SpaceShip((100,100))
+        SpaceShip((150,150))
+        SpaceShip((200,50))
+
     def step(self):
         for ship in self.getSpritesbyClass(SpaceShip):
-            ship.step()
-        
+            ship.step()        
+
 myapp = SpaceGame(SCREEN_WIDTH, SCREEN_HEIGHT)
 myapp.run()
